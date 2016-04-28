e3a2166a9bf799f4c64e09e122b638e1b2942f03
Japheth Obala <japhethobala@gmail.com>
2016-04-28 07:49:26 -0400

    Create Index

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,23 +1,34 @@
 /**
  * Created by japheth on 4/23/16.
  */
-describe("inverted index", function(){
+describe('Inverted Index', function(){
     var invertedIndex;
 
     beforeEach(function(done){
         invertedIndex = new InvertedIndex();
+        invertedIndex.index = [];
         invertedIndex.loadData('./books.json').done(function(data){
             invertedIndex.data = data;
-            console.log(invertedIndex.data);
             done();
-        })
+        });
     });
 
-   describe("read book data", function(){
-        it("checks that json file is not empty", function(){
+   describe('Read book data', function(){
+        it('Checks that json file is not empty', function(){
             expect(invertedIndex.data).toBeDefined();
             expect(invertedIndex.data.length).not.toEqual(0);
         })
-   })
+   });
 
+   describe('Populate Index', function(){
+       it("Checks if index is created", function(){
+           var index = invertedIndex.createIndex(invertedIndex.data);
+           expect(index).toBeDefined();
+       });
+       it("Checks if string keys map to the correct object in the object array", function(){
+           var index = invertedIndex.createIndex(invertedIndex.data);
+           expect(index[0]['keys']).toContain('alice');
+           expect(index[1]['keys']).toContain('rings');
+       });
+   });
 });
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -2,12 +2,33 @@
 
 class InvertedIndex {
 
-    constructor(){
+    constructor() {
         this.data = [];
     }
 
-    loadData(filePath){
-        return ($.getJSON(filePath))
+    loadData(filePath) {
+        return ($.getJSON(filePath));
+    }
+
+
+    createIndex(data) {
+        var index = [];
+        data.forEach(function (element) {
+            var keys = Object.keys(element);
+            var concat = " ";
+
+            keys.forEach(function(key){
+                concat += ' ' + element[key];
+            });
+            var normalised = concat.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(" ");
+            index.push({'title': element['title'], 'keys': normalised});
+        });
+
+        return index
     }
 }
+
+
+
+
 
