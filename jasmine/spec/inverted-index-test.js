61c95894475684135ab140807b4a049a8042ec09
Japheth Obala <japhethobala@gmail.com>
2016-05-07 16:33:59 -0400

    implement js style guide

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,72 +1,73 @@
 /**
  * Created by japheth on 4/23/16.
  */
-describe('Inverted Index', function(){
-    var invertedIndex;
+describe('Inverted Index', function() {
+  var invertedIndex;
 
-    beforeEach(function(){
-        invertedIndex = new InvertedIndex();
-        expect(invertedIndex.index).toEqual({});
-        invertedIndex.data = invertedIndex.loadData('books.json');
-        invertedIndex.createIndex(invertedIndex.data);
-        invertedIndex.index = invertedIndex.getIndex();
+  beforeEach(function() {
+    invertedIndex = new InvertedIndex();
+    expect(invertedIndex.index).toEqual({});
+    invertedIndex.data = invertedIndex.loadData('books.json');
+    invertedIndex.createIndex(invertedIndex.data);
+    invertedIndex.index = invertedIndex.getIndex();
+  });
 
+  describe('Read book data', function() {
+    it('checks that json file is not empty', function() {
+      expect(invertedIndex.data.length).not.toEqual(0);
     });
 
-   describe('Read book data', function(){
-        it('checks that json file is not empty', function(){
-            expect(invertedIndex.data.length).not.toEqual(0);
+    it('ensures each object in json array contain a property whose value is a string', function(){
+      for (var obj in invertedIndex.data) {
+        var keys = Object.keys(invertedIndex.data[obj]);
+        keys.forEach(function(key) {
+          expect(typeof(invertedIndex.data[obj][key])).toEqual('string');
         });
-
-       it('ensures each object in json array contain a property whose value is a string', function(){
-           for(var obj in invertedIndex.data){
-               var keys = Object.keys(invertedIndex.data[obj]);
-               keys.forEach(function(key){
-                   expect(typeof(invertedIndex.data[obj][key])).toEqual('string');
-               });
-           }
-       });
-
-       it('throws an error when file is not found', function(){
-         expect(function(){
-         invertedIndex.loadData('not_exist.json');
-         }).toThrow(new Error('unable to open file'));
-       });
-
-       it('throws an error when file is an empty json file', function(){
-         expect(function(){
-         invertedIndex.loadData('empty.json');
-         }).toThrow(new Error('the file is empty'));
-       });
-
-       it('throws an error when the json file is invalid', function(){
-         expect(function(){
-          invertedIndex.loadData('invalid.json');
-         }).toThrow(new Error('invalid json file'));
-       });
-   });
-
-   describe('Populate Index', function(){
-       it('checks if index is created', function(){
-           expect(invertedIndex.index).toBeDefined();
-           expect(invertedIndex.index).not.toEqual({});
-       });
-
-       it('checks if string keys map to the correct object in the object array', function(){
-           expect(invertedIndex.index.alice).toEqual([0]);
-           expect(invertedIndex.index.rings).toEqual([1]);
-           expect(invertedIndex.index.a).toEqual([0, 1]);
-       });
-   });
-    describe('Search Index', function(){
-        it('returns the index of the object containing the searchIndex term', function(){
-            expect(invertedIndex.searchIndex('alice')).toEqual([0]);
-            expect(invertedIndex.searchIndex('alice has been to wonderland')).toEqual([0, -1, -1, 1, 0]);
-            expect(invertedIndex.searchIndex('loRd Alice')).toEqual([1, 0]);
-            expect(invertedIndex.searchIndex(['rings'])).toEqual([1]);
-            expect(invertedIndex.searchIndex(['alice', 'rings'])).toEqual([0, 1]);
-            expect(invertedIndex.searchIndex('a')).toEqual([0, 1]);
-            expect(invertedIndex.searchIndex(['Japheth'])).toEqual([-1]);
-        })
+      }
     });
+
+    it('throws an error when file is not found', function() {
+      expect(function() {
+        invertedIndex.loadData('not_exist.json');
+      }).toThrow(new Error('unable to open file'));
+    });
+
+    it('throws an error when file is an empty json file', function() {
+      expect(function() {
+        invertedIndex.loadData('empty.json');
+      }).toThrow(new Error('the file is empty'));
+    });
+
+    it('throws an error when the json file is invalid', function() {
+      expect(function() {
+        invertedIndex.loadData('invalid.json');
+      }).toThrow(new Error('invalid json file'));
+    });
+  });
+
+  describe('Populate Index', function() {
+    it('checks if index is created', function() {
+      expect(invertedIndex.index).toBeDefined();
+      expect(invertedIndex.index).not.toEqual({});
+    });
+
+    it('checks if string keys map to the correct object in the object array', function() {
+      expect(invertedIndex.index.alice).toEqual([0]);
+      expect(invertedIndex.index.rings).toEqual([1]);
+      expect(invertedIndex.index.a).toEqual([0, 1]);
+    });
+  });
+
+  describe('Search Index', function() {
+    it('returns the index of the object containing the searchIndex term', function() {
+      expect(invertedIndex.searchIndex('alice')).toEqual([0]);
+      expect(invertedIndex.searchIndex('alice has been to wonderland')).toEqual([0, -1, -1, 1, 0]);
+      expect(invertedIndex.searchIndex('loRd Alice')).toEqual([1, 0]);
+      expect(invertedIndex.searchIndex(['rings'])).toEqual([1]);
+      expect(invertedIndex.searchIndex(['alice', 'rings'])).toEqual([0, 1]);
+      expect(invertedIndex.searchIndex('a')).toEqual([0, 1]);
+      expect(invertedIndex.searchIndex(['Japheth'])).toEqual([-1]);
+    });
+  });
 });
+
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -2,13 +2,13 @@
 
 class InvertedIndex {
 
-    constructor() {
-        this.data = [];
-        this.index = {};
-    }
+  constructor() {
+    this.data = [];
+    this.index = {};
+  }
 
-    // Loads data from file in the path given
-    loadData(filepath){
+  // Loads data from file in the path given
+  loadData(filepath) {
     var request = new XMLHttpRequest();
       request.open('GET', filepath, false);
       request.send(null);
@@ -23,87 +23,90 @@ class InvertedIndex {
         }
       }
       throw new Error('unable to open file');
-    }
+  }
 
     /**
      * Takes data from a json file an returns an index object
      * @param data
      * @returns {{}|*}
      */
-    createIndex(data) {
-        for(var doc = 0; doc < data.length; doc++){
+  createIndex(data) {
+    for (var doc = 0; doc < data.length; doc++) {
 
-            // get the unique words from the object content
-            var words = this.getUniqueWords(data[doc]);
+      // get the unique words from the object content
+      var words = this.getUniqueWords(data[doc]);
 
-            for(var word in words){
-                // if word is not in index assign it the value of the object's position
-                if (!(words[word] in this.index)){
-                    this.index[words[word]] = [doc];
-                } else {
-                    // else push the other objects position.
-                    // ie if it was {a: [0]}, now will be {a: [0, 1]}
-                    this.index[words[word]].push(doc);
-                }
-            }
+      for (var word in words) {
+        
+        // if word is not in index assign it the value of the object's position
+        if (!(words[word] in this.index)) {
+          this.index[words[word]] = [doc];
+        } else {
+
+          // else push the other objects position, ie if it was {a: [0]} now will be {a: [0, 1]}
+          this.index[words[word]].push(doc);
         }
-
+      }
     }
 
-    getIndex(){
-        return this.index;
+  }
+
+  getIndex() {
+    return this.index;
+  }
+
+  /**
+  * Takes a term or list of terms and returns the index of the document its present in
+  * @param terms
+  * @returns {Array} an array of results
+  */
+  searchIndex(terms) {
+    var termsArray = [];
+    if (typeof(terms) === 'string') {
+            
+    // if the search term is a string, break it down to an array of unique words
+    termsArray = this.processText(terms);
+    } else {
+      termsArray = terms;
     }
-
-    /**
-     * Takes a term or list of terms and returns the index of the document its present in
-     * @param terms
-     * @returns {Array} an array of results
-     */
-    searchIndex(terms){
-        var termsArray = [];
-        if(typeof(terms) === 'string'){
-            // if the search term is a string, break it down to an array of unique words
-            termsArray = this.processText(terms);
-        }else{
-            termsArray = terms;
-        }
-        var results = [];
-        for(var term = 0; term < termsArray.length; term++){
-            // check if the term is in the index
-            if(termsArray[term] in this.index){
-                // if it is push it value to the results array
-                results = results.concat(this.index[termsArray[term]]);
-            }else{
-                // if it's not push -1 into the results array
-               results = results.concat([-1]);
-            }
-        }
-        return results;
+    var results = [];
+    for (var term = 0; term < termsArray.length; term++) {
+      
+      // check if the term is in the index
+      if (termsArray[term] in this.index) {
+        
+        // if it is push it value to the results array
+        results = results.concat(this.index[termsArray[term]]);
+        } else {
+        
+        // if it's not push -1 into the results array
+        results = results.concat([-1]);
+      }
     }
+    return results;
+  }
 
 
-    /**
-     * It takes a book object an returns an array of unique terms
-     * @param bookDocument a book object
-     * @returns {Array} array of unique words
-     */
-    getUniqueWords(bookDocument){
+  /**
+  * It takes a book object an returns an array of unique terms
+  * @param bookDocument a book object
+  * @returns {Array} array of unique words
+  */
+  getUniqueWords(bookDocument) {
         var content = bookDocument.title +' '+ bookDocument.text;
         return this.processText(content);
-    }
+  }
 
-    /**
-     * It takes a book object's content and returns an array of unique values
-     * @param text  contents of the book object
-     * @returns {Array} array of unique words
-     */
-    processText(text){
+  /**
+  * It takes a book object's content and returns an array of unique values
+  * @param text  contents of the book object
+  * @returns {Array} array of unique words
+  */
+  processText(text) {
         var normalised = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase().split(' ');
-        // var names = ['prosper', prosper]
-        // var uniqueProsper = new Set(names)
-        // uniqueProsper = {'prosper'}
         var uniqueWords = new Set(normalised);
         return Array.from(uniqueWords);
 
-    }
+  }
 }
+
